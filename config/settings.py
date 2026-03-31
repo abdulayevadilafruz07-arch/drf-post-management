12c6277cdbc79e86402d9608752e877ad63eb20f
dilafruz <dilafruz@gmail.com>
2026-03-31 12:45:22 +0500

    deploy

--- config/settings.py
+++ config/settings.py
@@ -9,12 +9,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1cp5@@&q-*cvqm9(c-nplda%f%hjqi*x^9g4#f^&n97ga%d(@6'
+# SECRET_KEY = 'django-insecure-1cp5@@&q-*cvqm9(c-nplda%f%hjqi*x^9g4#f^&n97ga%d(@6'
+
+import os
+from dotenv import load_dotenv
+
+# .env faylidagi o'zgaruvchilarni yuklash
+load_dotenv()
+secret_key = os.getenv('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -105,10 +113,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
